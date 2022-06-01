07ac867525aad376f99261cd49b72159b30ad32a
romaing <rocambille@gmail.com>
2022-06-01 15:48:22 +0200

    added user post in frontend

--- frontend/src/pages/Users.jsx
+++ frontend/src/pages/Users.jsx
@@ -1,5 +1,38 @@
+import { useRef } from "react";
+
 import UserList from "@components/UserList";
 
 export default function Users() {
-  return <UserList />;
+  const loginRef = useRef();
+  const passwordRef = useRef();
+
+  return (
+    <>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+
+          const user = {
+            login: loginRef.current.value,
+            password: passwordRef.current.value,
+          };
+
+          fetch("http://localhost:5000/users", {
+            method: "post",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+          }).then((response) => {
+            alert(response.status);
+          });
+        }}
+      >
+        <input ref={loginRef} type="text" name="login" />
+        <input ref={passwordRef} type="password" name="password" />
+        <button type="submit">envoyer</button>
+      </form>
+      <UserList />
+    </>
+  );
 }
